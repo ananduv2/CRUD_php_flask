7ebe8eb91462f552841810d573de3eb4b66d4106
ananduv2 <ananduv2.0@gmail.com>
2022-04-19 16:23:58 +0530

    sql added in cloud

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_PASSWORD'] = 'nan123#'
 app.config['MYSQL_DB'] = 'demo'
 
 mysql = MySQL(app)
